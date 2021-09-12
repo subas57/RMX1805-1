4f82267ead8ee8d230225db651acf5427cd0628f
Jaideep <62387550+jaideepgithub@users.noreply.github.com>
2021-09-12 21:37:39 +0530

    RMX1805: Let's blissify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_RMX1805.mk
+    $(LOCAL_DIR)/bliss_RMX1805.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_RMX1805-user \
-    aosp_RMX1805-userdebug \
-    aosp_RMX1805-eng
+    bliss_RMX1805-user \
+    bliss_RMX1805-userdebug \
+    bliss_RMX1805-eng
