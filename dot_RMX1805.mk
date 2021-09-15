b72b5e8915571db099db01e7f1fcfbd3d045d9c3
Subas <77564932+subas57@users.noreply.github.com>
2021-09-15 09:08:19 +0530

    Update and rename aosp_RMX1805.mk to dot_RMX1805.mk

--- dot_RMX1805.mk
+++ dot_RMX1805.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common_full_phone.mk)
 
 # Inherit from RMX1805 device
 $(call inherit-product, device/oppo/RMX1805/device.mk)
@@ -14,7 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 PRODUCT_BRAND := oppo
 PRODUCT_DEVICE := RMX1805
 PRODUCT_MANUFACTURER := oppo
-PRODUCT_NAME := aosp_RMX1805
+PRODUCT_NAME := dot_RMX1805
 PRODUCT_MODEL := Realme 2/C1
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
@@ -24,7 +24,7 @@ TARGET_VENDOR_PRODUCT_NAME := RMX1805
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX1805"
 
-TARGET_GAPPS_ARCH := ARM64
+#TARGET_GAPPS_ARCH := ARM64
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_USES_BLUR := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
